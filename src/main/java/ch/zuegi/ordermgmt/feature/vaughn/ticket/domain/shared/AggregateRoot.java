0fb8d55a3db382d05f4b10af7abe5b461bd1d8c8
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2022-11-06 15:51:26 +0100

    # vaughn * loesche unbenutzte AggregateRoot Klasse

--- src/main/java/ch/zuegi/ordermgmt/feature/vaughn/ticket/domain/shared/AggregateRoot.java
+++
@@ -1,9 +0,0 @@
-package ch.zuegi.ordermgmt.feature.vaughn.ticket.domain.shared;
-
-public abstract class AggregateRoot<E, ID> extends Entity {
-
-    protected AggregateRoot(ID aggregateId) {
-        super(aggregateId);
-    }
-
-}
